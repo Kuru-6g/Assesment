256cef4f88e8cc64913c1aeb0511101de09bc505
Gurutharshan Mahendiran <62455117+guru06m@users.noreply.github.com>
2023-12-25 10:23:56 +0530

    Implemented OpenAI API for generating answers from PDF content

--- app.py
+++ app.py
@@ -6,13 +6,14 @@ app = Flask(__name__)
 
 
 def process_pdf(file):
-
     processed_text = llamaindex.process_pdf(file)
     return processed_text
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/query-pdf', methods=['POST'])
 def query_pdf():
@@ -22,8 +23,18 @@ def query_pdf():
     file = request.files['pdf_file']
     question = request.form['question']
 
-
     pdf_text = process_pdf(file)
+
+    response = openai.Answer.create(
+        documents=[pdf_text],
+        question=question,
+        search_model="davinci",
+        model="curie",
+        examples_context="In 2017, U.S. life expectancy was 78.6 years.",
+        examples=[["What is human life expectancy in the United States?", "78 years."]]
+    )
+
+    return response['answers'][0]
 
 
 if __name__ == '__main__':
