adef6b229debc007d5f9b84f8a73b34faab0cad9
Gurutharshan Mahendiran <62455117+guru06m@users.noreply.github.com>
2023-12-25 17:48:40 +0530

    Implemented basic PDF content manipulation function

---
+++ llamaindex.py
@@ -0,0 +1,8 @@
+
+def process_pdf(pdf_content):
+    # Simulated function to process PDF content using Llama Index
+    processed_text = pdf_content.upper()  # Example: Convert text to uppercase
+
+    # Perform more processing as needed by Llama Index
+
+    return processed_text
